0d2f32de98c7a79ca40b2b1a0c73de0ea099aade
master1977 <74092668+thaidev1502@users.noreply.github.com>
2021-04-05 18:08:33 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,80 +1,60 @@
-<?php
-$conn_string= "host=ec2-23-22-191-232.compute-1.amazonaws.com port=5432 dbname=d5kg68i2c9e8e1 user=gypewunfvnsgyy password=11c74095e3c4e49f75ba46bc2cdfa54b764e2a8f50ea90a047649e77ef517d3c"; 
-$dbconn= pg_connect($conn_string);
-if (isset($_POST['username'])) {
-	$username=$_POST['username'];
-
-}
-if (isset($_POST['password'])) {
-	$password=$_POST['password'];
-}
-$result= pg_query($dbconn, "SELECT * FROM tbl_user");
-while ($row=pg_fetch_row($result)) {
-	if ($row[0]==$username && $row[1]==$password) {
-		echo "Successfull";
-	}else{
-		echo ".......";
-	}
-}
+<?php 
+include("inc/header.php");
+include("inc/conn.php");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset-uft-8">
-	<meta name="viewport" content="witdh-device-witdh, initial-scale-l">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<title></title>
 </head>
 <body>
-	<style type="text/css">
-		html, body{
-	       font-family: 'Raleway';
-	       width: 100%;
-	       height: 100%;
-	       font-size: 16px;
-	       font-weight: bold;
-        }
-        .bg{
-	        background: url('https://assets-staging.audionetwork.com/usr/common/Background%20music%20header_745px.jpg');
-	        background-size: cover;
-	        width: 100%;
-	        height: 100%;
+<div class="container">
+	<div class="row mt-5">
+		
+		<div class="list-product-subtitle">
+			<h3 class="text-primary" ><a href="index.php">Top Hit<a></h3>
+		</div>
+		<br>
+		<div class="product-group">
+			<div class="row">
+				<?php
 
-        }
-        .row-container{
-	        border: 1px solid #ed6dcb;
-	        border-radius: 20px;
-	        margin-top: 20vh;
-	        padding:30px;
-        }
-        .lable, .form-check-label, h1{
-	        color: #f0f7f5;
-	        text-shadow: 2px 2px 10px black;
-        }
-	</style>
-<div class="container-fluid bg">
-	<div class="row justify-content-center">
-		<div class="col-md-3 col-sm-6 col-xs-12 row-container">
-			<form method="POST" action="">
-				<h1 style="text-align: center;">Login</h1>
-				<div class="form-group">
-					<label for="username">Username: </label>
-					<input type="username" class="form-control" id="username" placeholder="Enter username" name="username">
-				</div>
-				<div class="form-group">
-					<label for="pass">Password:</label>
-                    <input type="password" class="form-control" id="pass" placeholder="Enter password" name="password">
-				</div>
-				<div class="form-group form-check">
-					<label class="form-check-label">
-                      <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                    </label>
-				</div>
-				<button type="submit" class="btn btn-success btn-block">Login</button>
+		            if (!empty($_GET['page'])) {
+		    	        $page=$_GET['page']-1;
+		            }else{
+		    	        $page=0;
+		            }
+		    $product_per_page=8;
+		    $offset=$product_per_page*$page;
+		
+				$sql = "SELECT * FROM product LIMIT $offset, $product_per_page";
+                $query = mysqli_query($conn, $sql);
+				 while ($row = mysqli_fetch_array($query)) {  ?>
 				
-			</form>
-    </div>
+				<div class="col-md-3 col-sm-6 col-12">
+
+					<div class="card card-product mb-3" style="text-align: center;width: 100%">
+					  <img  class="card-img-top" style="width: 100%;" src="img/<?php echo $row['image'] ?>" alt="Card image cap" >
+					  <div class="card-body">
+					    <h5 class="card-title"><kbd><?php echo $row['proName']  ?></kbd></h5>
+					    <p class="card-text"><?php echo $row['proPrice'] ?></p>
+					    <a href="single-product.php?id=<?= $row['proID']?>" class="btn btn-primary">Detail</a>
+					  </div>
+					</div>
+				</div>
+				
+				<?php } ?>
+			
+			</div>
+		</div>
+
+		<br>
+
+	</div>
 </div>
 </body>
 </html>
+<?php
+include('inc/pagination.php');
+include('inc/footer.php');
 ?>
